01f8de5bf55f1207aebb431520416dabe468e562
Bruno Zutim <bzutim@gmail.com>
2021-09-28 09:56:47 -0300

    refactor: return count

--- src/controllers/userController/list.ts
+++ src/controllers/userController/list.ts
@@ -1,15 +1,22 @@
 import { Request, Response } from "express";
 
 import prisma from "../../databases/prisma/connection";
-import { userSelect } from "./utils/select";
+import { userPrismaSelect } from "./utils/select";
 
 export default async function list(req: Request, res: Response) {
   try {
-    const users = await prisma.user.findMany({
-      select: userSelect,
+    const countPromise = prisma.user.count();
+
+    const usersPromise = prisma.user.findMany({
+      select: userPrismaSelect,
     });
 
-    return res.json(users);
+    const [count, users] = await prisma.$transaction([
+      countPromise,
+      usersPromise,
+    ]);
+
+    return res.json({ count, users });
   } catch (error) {
     console.log(error);
     return res.status(400).json({ error });
