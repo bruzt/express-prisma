7a6554373339e8b10b5b9791db8438bf999e9678
Bruno Zutim <bzutim@gmail.com>
2021-09-30 14:55:30 -0300

    refactor: address soft delete

--- src/databases/prisma/middlewares/softDelete.ts
+++ src/databases/prisma/middlewares/softDelete.ts
@@ -1,7 +1,10 @@
 import { Prisma } from "@prisma/client";
 
 export default async function softDelete(params: Prisma.MiddlewareParams) {
-  if (params.model == "User" && params.action == "delete") {
+  if (
+    (params.model == "User" && params.action == "delete") ||
+    (params.model == "Addresses" && params.action == "delete")
+  ) {
     params.action = "update";
     params.args["data"] = { deleted_at: new Date() };
   }
