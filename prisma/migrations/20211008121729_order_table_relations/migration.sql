671bd2e0be66e70379c367a9b21948c1abb29a94
Bruno Zutim <bzutim@gmail.com>
2021-10-08 09:18:17 -0300

    chore: order table relations

---
+++ prisma/migrations/20211008121729_order_table_relations/migration.sql
@@ -0,0 +1,5 @@
+-- AddForeignKey
+ALTER TABLE "Order" ADD CONSTRAINT "Order_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Order" ADD CONSTRAINT "Order_address_id_fkey" FOREIGN KEY ("address_id") REFERENCES "Addresses"("id") ON DELETE SET NULL ON UPDATE CASCADE;
